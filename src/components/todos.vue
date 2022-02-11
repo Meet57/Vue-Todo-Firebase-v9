61f182793cb4d0c73f330420609eca6eb40ecb39
meet57 <meet57.mp@gmail.com>
2022-02-11 18:35:57 +0530

    converting css tabs to ant tabs

--- src/components/todos.vue
+++ src/components/todos.vue
@@ -1,150 +1,66 @@
 <template>
     <div class="mt-2">
-        <!-- Filter Model -->
-        <!-- <model-slot v-if="model" class="center w-1/3">
-            <template #header>
-                Filter for {{ !tab ? "Incomplete Tasks" : "Complete Tasks" }}
-            </template>
-            <template #body>
-                <div class="m-2 flex flex-wrap" v-if="!tab">
-                    <div
-                        v-for="color in incompletedTaskColor"
-                        :key="color"
-                        :style="{ color: color }"
-                    >
-                        <input
-                            type="checkbox"
-                            v-model="completedFilter"
-                            class="mx-2"
-                            :name="color"
-                            :id="color"
-                            :value="color"
-                        />{{ color }}
-                    </div>
-                </div>
-                <div class="m-2 flex flex-wrap" v-else>
-                    <div v-for="color in completedTaskColor" :key="color" :style="{ color: color }">
-                        <input
-                            type="checkbox"
-                            v-model="completedFilter"
-                            class="mx-2"
-                            :name="color"
-                            :id="color"
-                            :value="color"
-                        />{{ color }}
-                    </div>
-                </div>
-                <button
-                    class="ml-2 border-red-400 border px-3 py-0.5 rounded transition-all text-red-500 hover:border-transparent hover:bg-red-500 hover:text-white focus:outline-none"
-                    type="cancel"
-                    v-on:click="model = false"
-                >
-                    Cancel
-                </button>
-            </template>
-        </model-slot> -->
-        <!-- <div
-            class="filterOption border-blue-400 p-2 px-4 border rounded-lg hover:bg-blue-400 hover:text-white"
-            v-on:click="
-                () => {
-                    model = !model;
-                }
-            "
-        >
-            Filter
-        </div> -->
-        <div class="text-lg text-blue-600">Filter For</div>
-        <a-select
-            class="w-1/3"
-            mode="multiple"
-            :placeholder="!tab ? 'Incomplete Tasks' : 'complete Tasks'"
-            @change="filterChange"
-        >
-            <a-select-option
-                :style="{ backgroundColor: i }"
-                v-for="i in !tab ? incompletedTaskColor : completedTaskColor"
-                :key="i"
-            >
-                {{ i }}
-            </a-select-option>
-        </a-select>
-        <!-- {{ completedTaskColor }} -->
-        <!-- Filter Model -->
-
-        <div class="border-b border-gray-200 dark:border-gray-700">
-            <ul class="flex flex-wrap -mb-px">
-                <li
-                    :class="
-                        !tab
-                            ? 'inline-block py-4 px-4 text-sm font-medium text-center text-blue-600 rounded-t-lg border-b-2 border-blue-600 active dark:text-blue-500 dark:border-blue-500'
-                            : 'inline-block py-4 px-4 text-sm font-medium text-center text-gray-500 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:text-gray-400 dark:hover:text-gray-300'
-                    "
-                    v-on:click="tab = false"
-                >
+        <a-tabs default-active-key="1">
+            <a-tab-pane key="1">
+                <template #tab>
                     Incomplete Tasks
                     <span
                         class="ml-2 bg-red-100 text-red-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-red-200 dark:text-red-900"
                     >
                         {{ incompletedTasks.length }}
                     </span>
-                </li>
-                <li
-                    :class="
-                        tab
-                            ? 'inline-block py-4 px-4 text-sm font-medium text-center text-blue-600 rounded-t-lg border-b-2 border-blue-600 active dark:text-blue-500 dark:border-blue-500'
-                            : 'inline-block py-4 px-4 text-sm font-medium text-center text-gray-500 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:text-gray-400 dark:hover:text-gray-300'
-                    "
-                    v-on:click="tab = true"
+                </template>
+                <!-- Table -->
+                <table v-if="incompletedTasks.length > 0" class="w-full rounded-full">
+                    <tr class="p-3 bg-gray-300">
+                        <th class="text-left px-5 py-3 w-1/5">ID</th>
+                        <th class="text-left px-5 py-3 w-2/5">TODO</th>
+                        <th class="text-left px-5 py-3 w-1/5">STATUS</th>
+                        <th class="text-left px-5 py-3 w-1/5">ACTIONS</th>
+                    </tr>
+                    <todo v-for="row in incompletedTasks" :key="row.id" :row="row" />
+                </table>
+                <div
+                    v-else
+                    class="bg-blue-100 w-1/5 text-center p-3 m-3 text-blue-800 text-xs font-semibold mr-2 rounded dark:bg-blue-200 dark:text-blue-800"
                 >
+                    No Todo Here
+                </div>
+                <!-- Table over -->
+            </a-tab-pane>
+            <a-tab-pane key="2">
+                <template #tab>
                     Complete Tasks
                     <span
                         class="bg-green-100 text-green-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-900"
                     >
                         {{ completedTasks.length }}
                     </span>
-                </li>
-            </ul>
-        </div>
-        <template v-if="tab">
-            <table v-if="completedTasks.length > 0" class="w-full rounded-full">
-                <tr class="p-3 bg-gray-300">
-                    <th class="text-left px-5 py-3 w-1/5">ID</th>
-                    <th class="text-left px-5 py-3 w-2/5">TODO</th>
-                    <th class="text-left px-5 py-3 w-1/5">STATUS</th>
-                    <th class="text-left px-5 py-3 w-1/5">ACTIONS</th>
-                </tr>
-                <todo v-for="row in completedTasks" :key="row.id" :row="row" />
-            </table>
-            <div
-                v-else
-                class="bg-blue-100 w-1/5 text-center p-3 m-3 text-blue-800 text-xs font-semibold mr-2 rounded dark:bg-blue-200 dark:text-blue-800"
-            >
-                No Todo Here
-            </div>
-        </template>
-        <template v-else>
-            <table v-if="incompletedTasks.length > 0" class="w-full rounded-full">
-                <tr class="p-3 bg-gray-300">
-                    <th class="text-left px-5 py-3 w-1/5">ID</th>
-                    <th class="text-left px-5 py-3 w-2/5">TODO</th>
-                    <th class="text-left px-5 py-3 w-1/5">STATUS</th>
-                    <th class="text-left px-5 py-3 w-1/5">ACTIONS</th>
-                </tr>
-                <todo v-for="row in incompletedTasks" :key="row.id" :row="row" />
-            </table>
-            <div
-                v-else
-                class="bg-blue-100 w-1/5 text-center p-3 m-3 text-blue-800 text-xs font-semibold mr-2 rounded dark:bg-blue-200 dark:text-blue-800"
-            >
-                No Todo Here
-            </div>
-        </template>
+                </template>
+                <!-- Table -->
+                <table v-if="completedTasks.length > 0" class="w-full rounded-full">
+                    <tr class="p-3 bg-gray-300">
+                        <th class="text-left px-5 py-3 w-1/5">ID</th>
+                        <th class="text-left px-5 py-3 w-2/5">TODO</th>
+                        <th class="text-left px-5 py-3 w-1/5">STATUS</th>
+                        <th class="text-left px-5 py-3 w-1/5">ACTIONS</th>
+                    </tr>
+                    <todo v-for="row in completedTasks" :key="row.id" :row="row" />
+                </table>
+                <div
+                    v-else
+                    class="bg-blue-100 w-1/5 text-center p-3 m-3 text-blue-800 text-xs font-semibold mr-2 rounded dark:bg-blue-200 dark:text-blue-800"
+                >
+                    No Todo Here
+                </div>
+                <!-- Table Over -->
+            </a-tab-pane>
+        </a-tabs>
     </div>
 </template>
 
 <script>
 import todo from "./todo.vue";
-// import { DownOutlined } from "@ant-design/icons-vue";
 export default {
     name: "AllTodos",
     components: {
