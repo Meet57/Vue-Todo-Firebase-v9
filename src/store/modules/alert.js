60d0bc7a598b5d27c505e0ded336fee0ce7081b4
meet57 <meet57.mp@gmail.com>
2022-03-04 17:11:43 +0530

    alert

--- src/store/modules/alert.js
+++ src/store/modules/alert.js
@@ -9,14 +9,19 @@ export const getters = {
 };
 
 export const mutations = {
-    alert(state, text) {
-        state.alert = text;
+    alert(state, payload) {
+        state.alert = paylaod.text;
+        console.log(payload.text);
+        setTimeout(() => {
+            state.alert = "";
+        }, 3000);
     },
 };
 
 export const actions = {
     alert({ commit }, payload) {
         commit("alert", payload.text);
+        console.log(payload.text);
         setTimeout(() => {
             commit("alert", "");
         }, 3000);
